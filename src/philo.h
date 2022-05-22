d81b1ca060b0a6be3e5ab59dafd913b345427bca
bnidia <bnidia@student.21-school.ru>
2022-05-22 16:31:57 +0300

    Заготовка главной функции.
    Примерный план, что будет происходить дальше:
    * Функция init будет отвечать за инициализацию параметров
    * Функция simulation будет делать основную симуляцию
    * Функция memory_clearing будет очищать память

--- src/philo.h
+++ src/philo.h
@@ -22,19 +22,39 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_setup
+typedef struct s_main
 {
-	int number_of_philosophers;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int number_of_times_each_philosopher_must_eat;
-}		t_setup;
+// Входные данные
+	int 			number_of_philosophers;
+	int 			time_to_die;
+	int 			time_to_eat;
+	int 			time_to_sleep;
+	int 			number_of_times_each_philosopher_must_eat;
+// Мьютексы на вилки, печать и смерть
+	pthread_mutex_t	*mtx_forks;
+	pthread_mutex_t	mtx_print;
+	pthread_mutex_t	mtx_dead;
+// Переменная на случай ошибок
+	int 			err;
+}				t_main;
 
 typedef struct s_philo
 {
-	int	id;
-}		t_philo;
+	int				id;
+	int				count_of_eat;
+	pthread_t		tid;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	mtx_eat;
+	long long int	last_time_eat;
+	t_main			*params;
+}				t_philo;
+
+// Основа программы
+int		init(t_main *m, int argc, char **argv);
+int 	simulation(t_main *m);
+int 	memory_clearing(t_main *m);
+
 
 long	get_time(void);
 void	ft_usleep(long time);
